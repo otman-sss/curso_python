836862bd0f0bc083dbc08913dbab19112328f082
otman <otman.gmqz@gmail.com>
2025-01-27 10:21:46 -0700

    app

--- linea/app.py
+++ linea/app.py
@@ -1,3 +1,4 @@
+import funciones
 def calcular_y(x, m, b):
     '''
     Calcula el valor de y en una linea recta
@@ -10,9 +11,16 @@ def calcular_y(x, m, b):
 def main():
     m=2
     b=3
-    x=5
-    y = calcular_y(x,m,b)
-    print(f'Para x = {x}, y = {y}')
+    X= [x for x in range(1,11)]
+    Y = [funciones.calcular_y(x,m,b) for x in X]
+    print("Enteros:")
+    coordenadas_enteros = list(zip(X,Y))
+    print(coordenadas_enteros)
+    XF = [x for x in range(1,11,0,5)]
+    YF = [funciones.calcular_y(x,m,b) for x in XF]
+    coordenadas_flotantes = list(zip(XF,YF))
+    print("Flotantes:")
+    print(coordenadas_flotantes)
     
 if __name__ =='__main__':
-    main()
+    main()  
