c841b802188ed0902923becf5b6dd1446cacc24d
otman <otman.gmqz@gmail.com>
2025-01-27 10:33:10 -0700

    Modifique calcular_y() pata aceptar solo flotantes

--- linea/app.py
+++ linea/app.py
@@ -9,14 +9,14 @@ def calcular_y(x, m, b):
     '''
     return m*x + b
 def main():
-    m=2
-    b=3
-    X= [x for x in range(1,11)]
-    Y = [funciones.calcular_y(x,m,b) for x in X]
-    print("Enteros:")
-    coordenadas_enteros = list(zip(X,Y))
-    print(coordenadas_enteros)
-    XF = [x for x in range(1,11,0,5)]
+    m=2.0
+    b=3.0
+  #  X= [x for x in range(1,11)]
+   # Y = [funciones.calcular_y(x,m,b) for x in X]
+    #print("Enteros:")
+    #coordenadas_enteros = list(zip(X,Y))
+    #print(coordenadas_enteros)
+    XF = [x/10.0 for x in range(10,110,5)]
     YF = [funciones.calcular_y(x,m,b) for x in XF]
     coordenadas_flotantes = list(zip(XF,YF))
     print("Flotantes:")
--- linea/funciones.py
+++ linea/funciones.py
@@ -1,5 +1,5 @@
 # Archivo con todas las funciones necesarias para la aplicacion "linea"
-def calcular_y(x, m, b):
+def calcular_y(x:float, m:float, b:float)-> float:
     '''
     Calcula el valor de y en una linea recta
     x: el valor de x
@@ -14,11 +14,11 @@ def test_linea():
     Prueba de funcionamiento de calcular_Y
     '''
 
-    y = calcular_y(0, 2, 3)
+    y = calcular_y(0.0, 2.0, 3.0)
     return y
 
 if __name__ == '__main__':
-    if test_linea() == 3:
+    if test_linea() == 3.0:
         print('Test exitoso')
     else:
         print('Test fallido')    
