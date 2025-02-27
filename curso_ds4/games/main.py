4943d2d87891320eb01a7c527e765e71432ca5b2
otman <otman.gmqz@gmail.com>
2025-02-27 10:50:57 -0700

    Agregamos el torneo entre 4 equipos

--- curso_ds4/games/main.py
+++ curso_ds4/games/main.py
@@ -14,13 +14,34 @@ def main(archivo_torneo:str):
     else:
         players_mexico = ['Chicharito', 'Chucky', 'Ochoa', 'Tecatito', 'Guardado', 'Herrera', 'Layun', 'Moreno', 'Araujo', 'Oribe', 'Jimenez'] 
         players_espania = ['Casillas', 'Ramos', 'Pique', 'Iniesta', 'Silva', 'Isco', 'Busquets', 'Costa', 'Moreta', 'Asensio']
+        
+        players_brasil = ['Neymar', 'Coutinho', 'Marcelo', 'Casemiro', 'Alisson', 'Jesus','Paulinho','Thiago', 'Silva', 'Firmino', 'Danilo']
+        players_argentina = ['Messi', 'Aguero', 'Di Maria', 'Mascherano', 'Higuain', 'Dybala', 'Otamendi', 'Romero','Rojo','Banega' ,'Perez' ]
+
         lista_mexico = [Athlete(x) for x in players_mexico]
         lista_espania = [Athlete(x) for x in players_espania]
+        lista_brasil = [Athlete(x) for x in players_brasil]
+        lista_argentina = [Athlete(x) for x in players_argentina]
         soccer = Sport("soccer", 11, "FIFA")
         mexico = Team("Mexico", soccer, lista_mexico)
-        espania = Team("espania", soccer, lista_espania)
-        juego = Game(mexico, espania)       
-        torneo = [juego.to_json()]
+        espania = Team("Espania", soccer, lista_espania)
+        brasil = Team("Brasil", soccer, lista_brasil)
+        argentina = Team("Argentina", soccer, lista_argentina)
+        equipos = [mexico,espania,brasil, argentina]
+
+        d = {}
+        for local in equipos:
+            for visitante in equipos:
+                if local != visitante:
+                    juego = Game(local, visitante)
+                    partido = f'{local} - {visitante}'
+                    partido_2 = f'{visitante} - {local}'
+                    if partido_2 not in d:
+                        d[partido] = juego.to_json()
+        #print(d.keys())
+        torneo = list(d.values())
+        #juego = Game(mexico, espania)       
+        #torneo = [juego.to_json()]
         archivo = "torneo.json"
         with(open(archivo, "w", encoding="utf8")) as f:
             json.dump(torneo,f,ensure_ascii=False, indent=4)
