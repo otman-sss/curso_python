37dede94b092ede83856010a6da8321b382e96eb
otman <otman.gmqz@gmail.com>
2025-03-10 11:04:29 -0700

    La pagina ya funciona

--- curso_ds4/libros_web/funciones.py
+++ curso_ds4/libros_web/funciones.py
@@ -1,16 +1,36 @@
-'''Archivo con las funciones necesarias para la Aplicacion Libro Web'''
+''' Archivo con las funciones necesarias de la Aplicación Libro Web'''
 import csv
 
-def lee_archivo_csv(archivo:str) -> list:
+def lee_archivo_csv(archivo:str)->list:
+    ''' Lee un archivo CSV y lo convierte en una lista de diccionarios '''
     with open(archivo, "r", encoding='utf8') as f:
         return [x for x in csv.DictReader(f)]
-    
-def crea_diccionario_titulos(lista:list) -> dict:
-    '''Crea un diccionario con los titulos como clave y el resto de los datos como valores'''
-    return {x['titulo']: x for x in lista}   
+
+def crea_diccionario(lista:list, llave:str)->dict:
+    ''' Crea un diccionario con la palabra "llave" como clave y el resto de los datos como valores '''
+    return {x[llave].lower(): x for x in lista}
+
+def busca_en_diccionario(diccionario:dict, palabra:str)->list:
+    ''' Busca palabra en llave de la lista de diccionarios '''
+    lista = []
+    palabra = palabra.lower()
+    for llave, libro in diccionario.items():
+        if palabra in llave.lower():
+            lista.append(libro)
+    return lista
+
+def libros_empiezan_con(lista:list, letra:str)->list:
+    ''' Busca libros que empiecen con letra '''
+    return [x for x in lista if x['title'].lower().startswith(letra.lower())]
 
 if __name__ == '__main__':
     archivo_csv = 'booklist2000.csv'
     lista_libros = lee_archivo_csv(archivo_csv)
-    diccionario_libros = crea_diccionario_titulos
-    (lista_libros)
+    diccionario_libros = crea_diccionario(lista_libros,'title')
+    resultado = busca_en_diccionario(diccionario_libros, 'rebels')
+    print(resultado)
+    diccionario_autores = crea_diccionario(lista_libros,'author')
+    resultado = busca_en_diccionario(diccionario_autores, 'Sandra')
+    print(resultado)
+    resultado = libros_empiezan_con(lista_libros, 'a')
+    print(f'Libros que empiezan con "A": {len(resultado)}')
